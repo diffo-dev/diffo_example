13529d6e70a34536050f472291b921292a148336
Matt Beanland <beanland@live.com.au>
2026-05-09 22:31:45 +0930

    reuse fix

--- .igniter.exs
+++ .igniter.exs
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 diffo_example contributors <https://github.com/diffo-dev/diffo_example/graphs.contributors>
+#
+# SPDX-License-Identifier: MIT
+
 # This is a configuration file for igniter.
 # For option documentation, see https://hexdocs.pm/igniter/Igniter.Project.IgniterConfig.html
 # To keep it up to date, use `mix igniter.setup`
